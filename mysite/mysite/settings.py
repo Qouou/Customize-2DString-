5ca388215e1a8a62b121e40ff7af29b0bceaa640
miga-666 <s108213029@mail1.ncnu.edu.tw>
2022-09-13 19:50:34 +0800

    the previous bug was not bug actually, so restore it

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,12 +120,15 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static_file")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static_file")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
 )
+
+#ghjkl
